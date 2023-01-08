0e7f10db9988a57b1cf7ee2e0c7c385fa5fcba17
Philip Hell <blvckbytes@gmail.com>
2023-01-08 15:51:22 +0100

    added empty default bodies to IConfigSection

--- src/main/java/me/blvckbytes/bbconfigmapper/sections/IConfigSection.java
+++ src/main/java/me/blvckbytes/bbconfigmapper/sections/IConfigSection.java
@@ -10,13 +10,13 @@ public interface IConfigSection {
   /**
    * Called to decide the type of Object fields at runtime,
    * based on previously parsed values of that instance, as
-   * it's patched one field at a time. Decideable fields are
+   * it's patched one field at a time. Decidable fields are
    * always read last, so that they have access to other,
    * known type fields in order to decide properly.
    * @param field Target field in question
    * @return Decided type, Object.class means skip
    */
-  Class<?> runtimeDecide(String field);
+  default @Nullable Class<?> runtimeDecide(String field) { return null; }
 
   /**
    * Called when a field wasn't found within the config and a default could be set
@@ -24,12 +24,14 @@ public interface IConfigSection {
    * @param field Target field name
    * @return Value to use as a default
    */
-  @Nullable Object defaultFor(Class<?> type, String field);
+  default @Nullable Object defaultFor(Class<?> type, String field) {
+    return null;
+  }
 
   /**
    * Called when parsing of the section is completed
    * and no more changes will be applied
    */
-  void afterParsing(List<Field> fields) throws Exception;
+  default void afterParsing(List<Field> fields) throws Exception {}
 
 }
