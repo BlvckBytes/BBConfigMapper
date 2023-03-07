e78b6c28091b978112754f27505e6372c9b1b4de
Philip Hell <blvckbytes@gmail.com>
2023-03-08 00:10:10 +0100

    fixed non-persisting anchor names and added it to an existing test

--- src/main/java/me/blvckbytes/bbconfigmapper/YamlConfig.java
+++ src/main/java/me/blvckbytes/bbconfigmapper/YamlConfig.java
@@ -69,6 +69,7 @@ public class YamlConfig implements IConfig {
     DUMPER_OPTIONS = new DumperOptions();
     DUMPER_OPTIONS.setProcessComments(true);
     DUMPER_OPTIONS.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+    DUMPER_OPTIONS.setAnchorGenerator(Node::getAnchor);
 
     YAML = new Yaml(new Constructor(loaderOptions), new Representer(DUMPER_OPTIONS), DUMPER_OPTIONS, loaderOptions);
   }
